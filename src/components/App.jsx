b3d77d1d50af3988cee6d77433c4c5ec597a562a
Vetik007 <boy0007@ukr.net>
2023-05-30 23:44:21 +0300

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,85 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import contacts from './data/contactsData.json';
+import css from './App.module.css';
+
+class App extends Component {
+  state = {
+    contacts: contacts,
+    filter: '',
+  };
+
+  // Добавление нового контакта в список контактов
+  addContact = contact => {
+    const isInContacts = this.state.contacts.some(
+      ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
+    );
+
+    if (isInContacts) {
+      alert(`${contact.name} is already in contacts`);
+      return;
+    }
+    this.setState(prevState => ({
+      contacts: [{ id: nanoid(), ...contact }, ...prevState.contacts],
+    }));
+  };
+
+  // Изменение значения фильтра
+  changeFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  // Получение отфильтрованных контактов
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  // Удаление контакта из списка
+  removeContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+      };
+    });
+  };
+
+  render() {
+    const visibleContacts = this.getVisibleContacts();
+    const { filter } = this.state;
+
+    return (
+      <div className={css.wrapper}>
+        <h1>Phonebook</h1>
+
+        <ContactForm onSubmit={this.addContact} />
+
+        <h2>Contacts</h2>
+        {this.state.contacts.length > 0 ? (
+         // Фильтр для отображения контактов
+          <Filter value={filter} onChangeFilter={this.changeFilter} />
+        ) : (
+          <p>There are no contacts in the phone book!</p>
+        )}
+        {this.state.contacts.length > 0 && (
+          // Список контактов
+          <ContactList
+            contacts={visibleContacts}
+            onRemoveContact={this.removeContact}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
+
